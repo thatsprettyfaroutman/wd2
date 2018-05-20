b88627c98f61eda5db79c7641bd95e1f371c7afa
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-05-20 15:59:32 +0300

    Release 2

--- client/codeSplitAssets/NyanCat.js
+++
@@ -1,9 +0,0 @@
-
-// TODO: remove this
-
-export default () => {
-  const stars = document.querySelectorAll('.stars')[0];
-  for (let i = 0; i < 12; i += 1) {
-    stars.innerHTML += '<div class="star"><span></span></div>';
-  }
-};
